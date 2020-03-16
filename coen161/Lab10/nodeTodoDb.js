8c6dbef8d45f309027fd2ae12909ad90ceeefd26
zacharyrobin <zrobin144@gmail.com>
2020-03-15 19:30:30 -0700

    lab10 complete

--- coen161/Lab10/nodeTodoDb.js
+++ coen161/Lab10/nodeTodoDb.js
@@ -4,16 +4,51 @@ const mysql = require('mysql');
 const config = {
   host: "dbserver.engr.scu.edu",
   user: "zrobin",
-  password: "password",
+  password: "Password1",
   database: "sdb_"
 };
 
+// Adding
 exports.addTodo = function (sessionId, todo, callback) {
   const con = mysql.createConnection(config);
-  // call con.connect();
+  con.connect(function(err) 
+  {   // con.connect() is called 
+    if (err) 
+    {
+      return callback(err);
+    }
+    console.log("Connection successful");
+    var sql = "INSERT INTO Todos (description, sessionId) VALUES (?,?);";
+    con.query(sql, [todo,sessionId], function(err, result) 
+    {
+      if (err){
+        throw err;
+      }
+      con.end();
+    });
+
+  });
+
 };
 
+// Getting
 exports.getTodos = function (sessionId, callback) {
   const con = mysql.createConnection(config);
-  // call con.connect();  
+  con.connect(function(err) 
+  {   // con.connect() is called 
+    if (err) 
+    {
+      return callback(err);
+    }
+    console.log("Connection successful");
+    var sql = "SELECT * FROM Todos WHERE sessionId = ?;"
+    con.query(sql,[sessionId], function(err, result) 
+    {
+      if (err) 
+      {
+        throw err;
+      }
+      con.end();
+    });
+  });
 };
